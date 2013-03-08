95f5afefc95cb9de5277a5952d6a249752044fcf
Jackson Flint-Gonzales <jackson.gonzales@gmail.com>
2013-03-08 10:37:40 -0800

    final changes

--- QuoteBoard/settings.py
+++ QuoteBoard/settings.py
@@ -129,7 +129,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'quotes',
-    'ajax_validation',
+    'django-ajax-validation',
 )
 
 # A sample logging configuration. The only tangible logging
