f2be8d91da6003398c768ca83f856682d6be5aa2
jacksongonzales <jackson.gonzales@gmail.com>
2013-05-20 17:26:18 -0600

    Update addscript.js

--- QuoteBoard/static/addscript.js
+++ QuoteBoard/static/addscript.js
@@ -13,31 +13,7 @@ $(function(){
     $('#command').replaceWith("<h1 id='command'>Just like that.</h1>");
   });
 });
-/*
-  $('#quoteform').submit(function(e){
-    console.log(e);
-    $(this).validate(function(e){
-      $.post($(this).attr('action'), $(this).serialize(), function(data, text){
-        //this is what happens if the form submissino is successful
-      $(this).valid(function(){
-        $('#myModal').modal('show');
-      });
 
-        console.log(data);
-      });
-
-    });
-  
-      e.preventDefault();
-  });
-*/
-
-/*  submitHandler: function(form){
-    
-    console.log('hiya');
-    $('#myModal').modal('show');
-    //form.submit();
-  } */
 
 
 
